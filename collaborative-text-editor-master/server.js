1284f03b23e1363e58f726abfbca8a01ea22f0c9
Amanda Maria <amanda.marry@hotmail.com>
2018-01-26 23:19:07 -0300

    Collaborative text editor
    https://blog.pusher.com/build-collaborative-text-editor-javascript/
    
    https://github.com/ankeetmaini/collaborative-text-editor

---
+++ collaborative-text-editor-master/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve our JavaScript, CSS and index.html
+app.use(express.static('./'));
+
+var pusher = new Pusher({
+  appId: 'INSERT_YOUR_APP_ID_HERE',
+  key: 'INSERT_YOUR_KEY_HERE',
+  secret:  'INSERT_YOUR_SECRET_HERE' 
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Listening at http://localhost:5000'));
