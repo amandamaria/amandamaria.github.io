d88b0695e812fbd7588b54b12a45b7b008765514
Amanda <amanda.marry@hotmail.com>
2020-08-01 13:12:06 -0300

    finalizado esquema de emissão de certificados

--- pcinclusao/certificados/certificado.js
+++ pcinclusao/certificados/certificado.js
@@ -1,27 +1,32 @@
 var url = 'https://spreadsheets.google.com/feeds/list/1DZDX0pLYGvQipaRk1RsEOZWxNmkxxy9a2zAER4vJpj4/1/public/full?alt=json';
 
 function verificarCertificado() {	
-	var cpf = $('#cpf').val();	
+	var codigo = $('#codigo').val();	
 	
 	$.getJSON(url,function(result){
 		var dados = result.feed.entry;
 		var valido = false;
+		var nome;
+		var cpfFormatado;
 		for(var i=0; i<dados.length; i++) {
-			var cpfNaLista = dados[i].gsx$cpfnumbers.$t;			
-			if(cpfNaLista === cpf) {
+			var codigoNaLista = dados[i].gsx$codigo.$t;			
+			if(codigoNaLista.toUpperCase() === codigo.toUpperCase()) {
+				nome = dados[i].gsx$nome.$t;
+				cpfFormatado = dados[i].gsx$cpf.$t;
 				valido = true;
 				break;
 			}
 		}
 		if(valido) {
-			alert('CERTIFICADO VALIDADO');
+			var mensagem = 'CERTIFICADO VÁLIDO PARA '.concat(nome).concat(' CPF Nº ').concat(cpfFormatado);
+			alert(mensagem);
 		} else {
-			alert('CERTIFICADO INVÁLIDO');
+			alert('CÓDIGO DE VERIFICAÇÃO NÃO IDENTIFICADO');
 		}
 	});
 }
 
-function gerarCertificado() {	
+function gerarCertificado(mainPage) {	
 	var cpf = $('#cpf').val();	
 	
 	$.getJSON(url,function(result){
@@ -35,7 +40,12 @@ function gerarCertificado() {
 			}
 		}
 		if(valido) {
-			var linkCertificado = '<a target="_blank" href="files/CERTIFICADO_####.pdf">BAIXAR CERTIFICADO</a>';
+			var linkCertificado = '<a target="_blank" class="btn btn-primary btn-learn" href="****/CERTIFICADO_####.pdf">BAIXAR CERTIFICADO</a>';
+			if(mainPage) {
+				linkCertificado = linkCertificado.replace('****','certificados/files');
+			} else {
+				linkCertificado = linkCertificado.replace('****','files');
+			}			
 			linkCertificado = linkCertificado.replace('####',cpf);
 			$('#divLinkCertificado').html(linkCertificado);			
 		} else {
