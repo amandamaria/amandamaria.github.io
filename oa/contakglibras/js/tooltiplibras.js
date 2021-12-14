1456f5da2dfd470b531789e4d89689971d035e0b
Amanda Maria <amanda.marry@hotmail.com>
2021-12-14 16:38:35 -0300

    Corrigida verificação do CSS de tooltiplibras

--- oa/contakglibras/js/tooltiplibras.js
+++ oa/contakglibras/js/tooltiplibras.js
@@ -2,7 +2,7 @@ $(document).on("mouseover", "[tooltip]", function() {
 	var gif = $(this).attr('tooltip');
 	var styles = Array.from(document.styleSheets);
 	styles.map(function(styleSheet){
-		if(styleSheet.href.includes("tooltiplibras-1.0.0.css")) {
+		if(styleSheet.href != null && styleSheet.href.includes("tooltiplibras-1.0.0.css")) {
 			styleSheet.addRule('[tooltip]::after','background-image: url("'+gif+'");')
 		}
 	});
